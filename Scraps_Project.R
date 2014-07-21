17b8608a363494a924128eaf6cbac721989d488b
jjf214 <fenstjj1@fenstjj1-ml2.jhuapl.edu>
2014-07-21 11:45:53 -0400

    Working on the training set

---
+++ Scraps_Project.R
@@ -0,0 +1,20 @@
+'%!in%' <- function(x,y)!('%in%'(x,y))
+is.blank <-function(x) (x == "")
+
+training = read.delim(file = "pml-training.csv",sep=",")
+attach(training)
+training = training[ ,colnames(training) %!in% c("X",
+                                                 "raw_timestamp_part_1",
+                                                 "raw_timestamp_part_2",
+                                                 "cvtd_timestamp",
+                                                 "new_window",
+                                                 "num_window")]
+
+keep1=names(training)[(colSums(is.na(training)/nrow(training))<.1)]
+training=training[ ,keep1]
+
+keep2=names(training)[(colSums(is.blank(training)/nrow(training))<.1)]
+training=training[, keep2]
+
+featurePlot(x=training[, c("classe","roll_belt","pitch_belt","yaw_belt")],
+            y=training[,"classe"])
